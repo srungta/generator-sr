9aa5dfa560e0bdfd63b75d6a80e7777bdf83888f
Shaswat Rungta <shrung@microsoft.com>
2020-09-13 03:41:04 +0530

    renamed to rfc

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,6 +3,6 @@
 const Generator = require('yeoman-generator');
 module.exports = class extends Generator {
     install() {
-        this.log('\nSupported actions : rcomp');
+        this.log('\nSupported actions : rfc');
     }
 };
